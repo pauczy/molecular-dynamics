d508d5e6f272a69cac8f1177a339c72e17b57144
Paulina Czyz <paula.czyz@gmail.com>
2021-10-25 18:27:59 +0200

    part 3 simulation

--- argon.py
+++ argon.py
@@ -1,8 +1,8 @@
 import numpy as np
-import sys
-import time
-import math
-from math import sqrt
+from sys import argv
+from time import time
+from math import sqrt, pi
+from numba import njit
 
 
 def calculate_positions(a, n, N):
@@ -17,7 +17,7 @@ def calculate_positions(a, n, N):
                 i = i0 + i1*n + i2*n*n
                 atoms[i] = (i0 - (n-1)/2)*b0 + (i1 - (n-1)/2)*b1 + (i2-(n-1)/2)*b2
 
-    np.savetxt('out_pos.txt', atoms, delimiter = '\t')
+    # np.savetxt('out_pos.txt', atoms, delimiter = '\t')
     return atoms
 
 def calculate_momenta(k, T_0, N, m):
@@ -26,87 +26,94 @@ def calculate_momenta(k, T_0, N, m):
     momSum = np.sum(momenta, axis = 0)
     momenta -= momSum/N
 
-    np.savetxt('out_mom.txt', momenta, delimiter = '\t')
+    #np.savetxt('out_mom.txt', momenta, delimiter = '\t')
     return momenta
 
-#potencjal ogolnia wartosc, sily dla wszystkich atomow wypisac
+@njit #  = @jit(nopython=True)
 def calculate_VFP(atoms, N, L, f, e, R):
-    V = 0.
-    F = np.zeros((N, 3))
 
-    #wall potential
-    V_s = np.zeros((N))
-    r = np.sqrt(np.sum(atoms**2, axis = 1))
-    V_s[r >= L] = 1./2*f*np.square(r-L)[r >= L]
-    V += np.sum(V_s, axis = 0)
+    V = 0
+    P = 0
+    F_S = np.zeros((N, 3))
+    F_P = np.zeros((N, N, 3))
 
-    #wall forces
-    F_s = np.zeros((N, 3))
-    F_s[r >= L] =f*(L-r[r >= L, np.newaxis])*atoms[r >= L]/r[r >= L, np.newaxis]
-    F += F_s
-
-    #van der waals potential + forces between atom pairs
-    V_vdw = np.zeros((N, N))
-    F_vdw = np.zeros((N, N, 3))
     for i in range(N):
+        ri = np.linalg.norm(atoms[i])
+        if ri >= L:
+            V += 1./2*f*(ri - L)**2
+            F_S[i] = f*(L - ri)*atoms[i]/ri
         for j in range(i):
-            rij = np.linalg.norm(atoms[i] - atoms[j])
-            V_vdw[i, j] = e * ( (R/ rij)**12 - 2*(R/rij)**6 )
-            F_vdw[i, j, :] = 12*e*((R/rij)**12 - (R/rij)**6)*(atoms[i]-atoms[j])/rij**2
-            F[i, :] += F_vdw[i, j, :]
-            F[j, :] -= F_vdw[i, j, :]
-    V += np.sum(V_vdw)
+            rij = np.linalg.norm(atoms[i]-atoms[j])
+            V += e*((R/rij)**12 - 2 * (R/rij)**6)
+            F_P[i, j] = 12 * e * ((R/rij)**12 - (R/rij)**6) * ((atoms[i] - atoms[j])/(rij**2))
+            F_P[j, i] = - F_P[i, j]
 
-    #temporary pressure
-    P = np.sum(np.sum(F_s**2, axis = 1), axis=0)/(4*math.pi*L**2)
+    F = np.sum(F_P, axis=1) + F_S
+    P = np.sum(np.sqrt(np.sum(F_S**2, axis = 1)), axis=0)/(4*pi*L**2)
 
-    print('V_s: ',  np.sum(V_s, axis=0))
-    print('V: ', V)
-    print('P: ', P)
-    np.savetxt('out_for.txt', F, delimiter = '\t')
-
+    # np.savetxt('out_for.txt', F, delimiter = '\t')
     return V, F, P
 
-def simulation(atoms, momenta, N, L, f, e, R, tau, m, k, So, Sd):
+
+def simulation(atoms, momenta, N, L, f, e, R, tau, m, k, So, Sd, S_out, S_xyz):
     V, F, P = calculate_VFP(atoms, N, L, f, e, R)
     Tavr = Havr = Pavr = 0
-    with open(sys.argv[2], 'w+') as outfile, open(sys.argv[3], 'w+') as outxyz:
+    with open(argv[2], 'w+') as out, open(argv[3], 'w+') as outxyz:
+        out.write("t \t\t H \t\t V \t\t T \t\t P \n")
         for s in range(1, So + Sd + 1):
+
                 momenta = momenta + 1./2*F*tau
                 atoms = atoms + 1./m * momenta*tau
                 V, F, P = calculate_VFP(atoms, N, L, f, e, R)
                 momenta = momenta + 1./2* F*tau
 
-                Ekin = np.sum(np.linalg.norm(momenta)**2 / (2*m), axis=0)
-                H = Ekin + V
-                T= 2 / (3*N*k)*Ekin
+                Ekin = np.linalg.norm(momenta, axis = 1)**2 / (2*m)
+                Ekin_sum = np.sum(Ekin, axis = 0)
+                H = Ekin_sum + V
+                T= 2 / (3*N*k)*Ekin_sum
 
-                if(s % S_out == 0):
+                if(s % S_out == 0): #TODO
+                    # zapis chwilowych charakterystyk t, H, V, T, P w formacie wykładniczym do out.txt
+                    out.write("{:.3f}".format(s*tau) + "\t")
+                    out.write("{:.5E}".format(H) + "\t")
+                    out.write("{:.5E}".format(V) + "\t")
+                    out.write("{:.5E}".format(T) + "\t")
+                    out.write("{:.5E}".format(P) + "\n")
 
                 if(s % S_xyz == 0):
+                    # zapis wsp. atomów oraz ich Ekin do avs.dat: xi, yi, zi, Ekini
+                    data_xyz = np.concatenate((atoms, Ekin[:, None]), axis = 1)
+                    np.savetxt(outxyz, data_xyz, delimiter = '\t')
+                    outxyz.write('\n\n')
 
                 if(s >= So):
+                    Tavr += T
+                    Pavr += P
+                    Havr += H
 
-
-
+    Tavr /= Sd
+    Pavr /= Sd
+    Havr /= Sd
+    print("Tavr: ", Tavr)
+    print("Pavr: ", Pavr)
+    print("Havr: ", Havr)
 
 
 if __name__ == "__main__":
 
-    tic = time.time()
+    tic = time()
 
     params = {}
-    with open(sys.argv[1]) as f:
+    with open(argv[1]) as f:
         for line in f:
             val, key = line.split("#")
             params[key.strip()] = float(val.strip())
     params['N'] = int(params['n']*params['n']*params['n'])
-    params['k'] = 0.00831
 
     atoms = calculate_positions(params['a'], int(params['n']), params['N'])
     momenta = calculate_momenta(params['k'], params['T_0'], params['N'], params['m'])
-    V, F, P = calculate_VFP(atoms, params['N'], params['L'], params['f'], params['e'], params['R'])
-    simulation(atoms, momenta, params['N'], params['L'], params['f'], params['e'], params['R'], params['tau'], params['m'], params['k'], int(params['So']), int(params['Sd']))
-    print(params['tau'])
-    toc = time.time()
-    print('execution time in ms: ', 1000*(toc-tic))
+    # V, F, P = calculate_VFP(atoms, params['N'], params['L'], params['f'], params['e'], params['R'])
+    simulation(atoms, momenta, params['N'], params['L'], params['f'], params['e'], params['R'], params['tau'], params['m'], params['k'], int(params['So']), int(params['Sd']), int(params['S_out']), int(params['S_xyz']))
+
+    toc = time()
+    print('execution time: ', (toc-tic))

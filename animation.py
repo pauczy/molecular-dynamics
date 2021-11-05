d5ea7ed069510ac74c03996c9406d73f67909057
Paulina Czyz <paula.czyz@gmail.com>
2021-11-05 16:37:24 +0100

    finished report

--- animation.py
+++ animation.py
@@ -10,8 +10,9 @@ import matplotlib.animation as animation
 n = 5
 L = 2.3
 timesteps = 1000
+coords_file = './output/avs_T1000.dat' #file with xyz positions of atoms
+data_file = './output/out_T1000.txt' #file with t. T, P, H data
 numP = n*n*n
-
 
 
 #sphere coordinates
@@ -22,12 +23,8 @@ xs = L*np.outer(np.sin(u), np.sin(v))
 ys = L*np.outer(np.sin(u), np.cos(v))
 zs = L*np.outer(np.cos(u), np.ones_like(v))
 
-
-# ax.plot_wireframe(x, y, z, rstride=3, cstride=3, linewidth=1)
-# ax.plot(atoms[:, 0], atoms[:, 1], atoms[:, 2], 'mo', markersize=5)
-
 #argon crystal
-with open('./avs_T500.dat', 'r') as f:
+with open(coords_file, 'r') as f:
     particleData = []
     for line in f:
         line = line.split()
@@ -37,24 +34,19 @@ x = [float(item[0]) for item in particleData]
 y = [float(item[1]) for item in particleData]
 z = [float(item[2]) for item in particleData]
 
-
 #time, temperature, pressure data
-data = np.loadtxt('./out_T500.txt', skiprows=1)
+data = np.loadtxt(data_file, skiprows=1)
 T = data[:, 3]
 P = data[:, 4]
 t = data[:, 0]
 
 
-
 fig = plt.figure()
 ax = p3.Axes3D(fig)
-
-
 border = ceil(L)
 ax.set_xlim3d([-border, border])
 ax.set_ylim3d([-border, border])
 ax.set_zlim3d([-border, border])
-
 
 def animate(i):
     ax.clear()
@@ -64,7 +56,7 @@ def animate(i):
     idx0 = i*numP
     idx1 = numP*(i+1)
     ax.plot_wireframe(xs, ys, zs, rstride=3, cstride=3, linewidth=1)
-    ax.scatter(x[idx0:idx1],y[idx0:idx1],z[idx0:idx1], c = 'm', s = 48, label = "T = " + str(T[i]) + "\nP = " + str(P[i]) + "\nt = " + str(t[i]))
+    ax.scatter(x[idx0:idx1],y[idx0:idx1],z[idx0:idx1], c = 'm', s = 360, label = "T = " + str(T[i]) + "\nP = " + str(P[i]) + "\nt = " + str(t[i]))
     ax.legend()
 
 ani = animation.FuncAnimation(fig, animate, frames=timesteps, interval=50, blit=False, repeat=False)
